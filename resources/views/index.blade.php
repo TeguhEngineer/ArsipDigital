82b0cf796b4ed484bd299c592fc44183ed796734
KH-Kurnyone <khikmalkurniawan@gmail.com>
2023-11-14 22:52:10 +0700

    eh

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,7 +57,6 @@
                         <li class="dropdown-header">
                             <h6>Desa Sukarasa</h6>
                             <span>Kec. Lakbok Kab. Pangandaran</span>
-
                         </li>
                         <li>
                             <hr class="dropdown-divider">
